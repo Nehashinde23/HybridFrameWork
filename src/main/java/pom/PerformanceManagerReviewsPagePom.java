8f990e1c408b925163c3d176d90364f38c2a41ce
Neha <nehashinde2303@gmail.com>
2024-08-23 13:36:31 +0530

    firstcommit

---
+++ src/main/java/pom/PerformanceManagerReviewsPagePom.java
@@ -0,0 +1,48 @@
+package pom;
+
+public class PerformanceManagerReviewsPagePom {
+	
+	//div[@class='oxd-topbar-header-title'] --performance manager review
+	
+	//span[text()='Configure ']--configure
+	
+	//li[@class='--active oxd-topbar-body-nav-tab --parent']//li[1]  --kpis
+	
+	//a[text()='Trackers'] --trackers
+	
+	//span[text()='Manage Reviews '] --manage reviews
+	
+	//a[text()='Manage Reviews']   --manage reviews
+	
+	//a[text()='My Reviews'] ---my reviews
+	
+	//a[text()='Employee Reviews']   ----employee reviews
+	
+	//a[text()='My Trackers']  ---mytrackers
+	
+	//a[text()='Employee Trackers']  --employee trackers
+	
+	//input[@placeholder='Type for hints...']  --employee name
+	
+	//(//div[contains(text(),'-- Select --')])[1]  ---job title
+	
+	//(//div[contains(text(),'-- Select --')])[2]   --sub unit
+	
+	//div[contains(text(),'Current Employees Only')] ---include
+	
+	//div[@clear='false']  ---review status
+	
+	//(//input[@placeholder='yyyy-dd-mm'])[1]   ---from date
+	
+	//(//input[@placeholder='yyyy-dd-mm'])[2]   ---todate
+	
+	//button[text()=' Reset ']  --reset button
+	
+	//button[text()=' Search '] --search button
+	
+	
+	
+	
+	
+
+}
